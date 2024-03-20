50be3bc724fd8a19697031aa84b366df51ba419b
YasminGuiniver <yasmin.patrocinio@sptech.school>
2024-03-20 11:22:56 -0300

    conta com novas informações

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 function calcularFinanceiro() {
 
-    var custoAnual = Number(gastosAnuaisInput.value);
+    var custoTotal = Number(gastosTotaisInput.value);
     var anoProjecao = Number(projecaoAnosInput.value);
 
-    var semSistema = custoAnual * anoProjecao;
-    var economia = ((17.86 / 100) * semSistema);
-    var comSistema = semSistema - economia;
+    var semSistema = custoTotal * anoProjecao;
+    var porcentagem = ((17.86 / 100) * custoTotal);
+    var porcentagemAnos = porcentagem * anoProjecao;
+    var comSistema = semSistema - porcentagem;
 
     resultadoDiv.innerHTML = `Sem o nosso sistema, em ${anoProjecao} anos, você gastaria um total de 
     <b>${semSistema.toFixed(2)} reais</b>. No entanto, com o nosso sistema, em uma projeção de 2 anos, 
     você irá gastar apenas <b>${comSistema.toFixed(2)} reais</b>, economizando significativamente 17,86% do valor 
-    total (<b>${economia.toFixed(2)} reais</b>).
+    total (<b>${porcentagemAnos.toFixed(2)} reais</b>).
     <br><br>
     <span>Insira seu e-mail para receber mais informações:</span>
     <div class="emailInputContainer">
